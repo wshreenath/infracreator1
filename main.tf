f0fea54ca484f7a81e62c30f5c62213d0df37227
wshreenath <148483485+wshreenath@users.noreply.github.com>
2025-01-09 08:02:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ region="us-east-1"
 resource "aws_instance" "one"{
 count=3
 ami="ami-0e2c8caa4b6378d8c"
-instance_type=t2.micro 
+instance_type=["t2.micro" ,"t2.medium" ,"t2.large"]
 tags ={
 
 Name=["dev-ser","test-ser","prod-ser"]
