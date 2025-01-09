793fb80ad8fa72b5341fd1529d81af35fef58fa0
wshreenath <148483485+wshreenath@users.noreply.github.com>
2025-01-09 07:58:47 +0530

    Create main.tf
    creating main.tf for terraform

---
+++ main.tf
@@ -0,0 +1,14 @@
+privider "aws"{
+
+region="us-east-1"
+}
+
+resource "aws_instance" "one"{
+count=3
+ami="ami-0e2c8caa4b6378d8c"
+instance_type=t2/micro 
+tags ={
+
+Name=["dev-ser","test-ser","prod-ser"]
+}
+}
